96d5a0019b35470d97420be321845814a245d0a7
JesuisRoti <romain.bouyx@viacesi.fr>
2019-01-07 15:47:13 +0100

    Creating database/migrations and administration for Products

--- inventaire/admin.py
+++ inventaire/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Produit
 
 # Register your models here.
+
+class ProduitAdmin(admin.ModelAdmin):
+    list_display =('nom', 'test')
+    list_filter = ('nom', 'test')
+    admin.site.site_header = 'Parametres'
+    admin.site.site_title = 'Parametres'
+    admin.site.index_title = 'FabLab'
+
+
+admin.site.register(Produit, ProduitAdmin)
---
+++ inventaire/static/js/fonctions.js
@@ -0,0 +1,4 @@
+function functionNON(id, quantite)
+{
+
+}
