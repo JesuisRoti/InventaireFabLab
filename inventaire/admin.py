182135077fd6892ce7e5929f13c3ed4ed20f64fd
JesuisRoti <romain.bouyx@viacesi.fr>
2019-01-10 15:47:06 +0100

    Front end changes/BDD CHANGES

--- inventaire/admin.py
+++ inventaire/admin.py
@@ -1,14 +1,21 @@
 from django.contrib import admin
-from .models import Produit
+from .models import *
 
 # Register your models here.
 
 class ProduitAdmin(admin.ModelAdmin):
-    list_display =('nom', 'test')
-    list_filter = ('nom', 'test')
+    list_display =('product_Name', 'product_Ref', 'available_Product', 'stock')
+    list_filter = ('product_Name',)
     admin.site.site_header = 'Parametres'
     admin.site.site_title = 'Parametres'
     admin.site.index_title = 'FabLab'
 
 
-admin.site.register(Produit, ProduitAdmin)
+admin.site.register(product, ProduitAdmin)
+admin.site.register(reservation)
+admin.site.register(category)
+admin.site.register(project_Reservation)
+admin.site.register(project_List)
+admin.site.register(stock_modification)
+admin.site.register(project_reservation_material)
+admin.site.register(project_material)
---
+++ inventaire/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import reservation
+from django.forms.widgets import HiddenInput
+
+class ReservationForm (forms.ModelForm):
+    class Meta:
+        model = reservation
+        widgets = {'id_Product': forms.HiddenInput()}
+        exclude = ('return_Date','return_Quantity',)
