c65d858f72274dfc4741a22843b509a4be272344
Quentin <quentin.desens@viacesi.fr>
2019-01-15 13:51:52 +0100

    Page erreur et succes

--- inventaire/urls.py
+++ inventaire/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('reservation/', views.reserver, name="reservation"),
     path('retour/', views.retour, name="retour"),
     path('error/', views.error, name="erreur"),
+    path('success/', views.success, name="succes"),
     path('', views.home_inventaire),
     path('pole/', views.show_pole),
     path('categorie/<str:categorie>', views.show_product),
