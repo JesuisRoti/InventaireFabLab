72f2d118c21428263317b2f5118b21f4c7a9738b
Mathieu <mathieu.froustey@viacesi.fr>
2019-01-15 09:09:14 +0100

    start side bar

--- inventaire/static/js/sidebar.js
+++ inventaire/static/js/sidebar.js
@@ -2,32 +2,32 @@ function myAccFunc() {
   var x = document.getElementById("demoAcc");
   if (x.className.indexOf("w3-show") == -1) {
     x.className += " w3-show";
-    x.previousElementSibling.className += " bleucesi";
+    x.previousElementSibling.className += " w3-pale-blue";
   } else {
     x.className = x.className.replace(" w3-show", "");
     x.previousElementSibling.className =
-    x.previousElementSibling.className.replace(" bleucesi", "");
+    x.previousElementSibling.className.replace(" w3-pale-blue", "");
   }
 }
 function myAccFunc2() {
   var x = document.getElementById("demoAcc2");
   if (x.className.indexOf("w3-show") == -1) {
     x.className += " w3-show";
-    x.previousElementSibling.className += " bleucesi";
+    x.previousElementSibling.className += " w3-pale-blue";
   } else {
     x.className = x.className.replace(" w3-show", "");
     x.previousElementSibling.className =
-    x.previousElementSibling.className.replace(" bleucesi", "");
+    x.previousElementSibling.className.replace(" w3-pale-blue", "");
   }
 }
 function myDropFunc() {
   var x = document.getElementById("demoDrop");
   if (x.className.indexOf("w3-show") == -1) {
     x.className += " w3-show";
-    x.previousElementSibling.className += " bleucesi";
+    x.previousElementSibling.className += " w3-pale-blue";
   } else {
     x.className = x.className.replace(" w3-show", "");
     x.previousElementSibling.className =
-    x.previousElementSibling.className.replace(" bleucesi", "");
+    x.previousElementSibling.className.replace(" w3-pale-blue", "");
   }
 }
