a8d7f041346f0df966c7f52b0ca5c61c0f86d6be
JesuisRoti <romain.bouyx@viacesi.fr>
2019-02-08 11:57:33 +0100

    Page 404

--- InventaireFabLab/settings.py
+++ InventaireFabLab/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=zs96v&%j_i8%x)pw(2m-ji8%n-b!ok7fvs8&jo5%tqvax_2nr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
